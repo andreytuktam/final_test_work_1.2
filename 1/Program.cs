de35e8d268ff4211774467853b2be3ff6e2da15d
Andrey Tuktam <tuktam@mail.ru>
2022-08-10 21:13:58 +0700

    объявили массив array

---
+++ 1/Program.cs
@@ -0,0 +1,8 @@
+﻿void FindStr()
+{
+    Console.WriteLine("Введите через пробел string эллементы массива:");
+    string s = Console.ReadLine();
+    string[] array = s.Split(' ');
+    
+}
+FindStr();
