938820b806835fb8173f7658a0ccd9773c6bdfea
Andrey Tuktam <tuktam@mail.ru>
2022-08-10 21:14:33 +0700

    объявили массив arraytwo

--- 1/Program.cs
+++ 1/Program.cs
@@ -3,6 +3,6 @@
     Console.WriteLine("Введите через пробел string эллементы массива:");
     string s = Console.ReadLine();
     string[] array = s.Split(' ');
-    
+    string[] arraytwo = new string[array.Length];
 }
 FindStr();
